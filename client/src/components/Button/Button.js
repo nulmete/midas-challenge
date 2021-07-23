4fd3fa65a2912194eed4aa246b8e40a3466b8d19
nicoulmete <nicolas.ulmete@mandiant.com>
2021-07-23 11:59:32 -0300

    fix button props

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ButtonStyles from './ButtonStyles';
 
-const Button = ({ children, centerX = false, ...props }) => (
-  <ButtonStyles type="button" centerX={centerX} {...props}>
+const Button = ({ children, type, centerX = false, ...props }) => (
+  <ButtonStyles type={type || 'button'} centerX={centerX} {...props}>
     {children}
   </ButtonStyles>
 );
@@ -11,6 +11,7 @@ const Button = ({ children, centerX = false, ...props }) => (
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   centerX: PropTypes.bool,
+  type: PropTypes.string,
 };
 
 export default Button;
